a0232e1472a9a493d9ee01ae8f17c9fc2d1cf90f
Festuskyalo254 <festusk625@gmail.com>
2025-11-14 11:46:33 +0300

    second commit

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Todo app running' });
+});
+const PORT = process.env.PORT || 5000;
+connectDB().then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+    });
